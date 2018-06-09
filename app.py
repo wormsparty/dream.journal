6260fc93d2a8690f1c3956b63fc98f5d872e164c
Marc-Olivier Bloch <wormsparty@gmail.com>
2018-06-09 19:35:32 +0200

    PoC

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import shelve
 
 app = Flask(__name__)
 
@@ -6,6 +7,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/get_subjects')
+def get_subjects():
+    d = shelve.open('database.db')
+
+    try:
+        return d['subjects']
+    except KeyError:
+        return []
+    finally:
+        d.close()
+
+
+@app.route('/add_subject')
+def add_subject(subject):
+    d = shelve.open('database.db')
+
+    try:
+        subjects = d['subjects']
+    except KeyError:
+        subjects = []
+
+    subjects.append(subject)
+    d['subjects'] = subjects
+    return 'OK'
 
 
 if __name__ == '__main__':
